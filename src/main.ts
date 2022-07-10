c848bf43269b9168b326ea132380d13ca5e99e4a
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-07-10 22:02:31 +0200

    working on it!!

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,30 @@ import "./style.css";
 type State = {
   word: string;
   characters: string[];
-}
+  maxMistakes: number;
+};
 let state: State = {
   word: "start",
-  characters: [`e`, `d`, `f`, `g`, `z`],
+  characters: [],
+  maxMistakes: 5,
 };
 const MAX_MISTAKES = 6;
 
 // Q: How do you get a random word?
 function getRandomWord(): string {
-  const words = ["apple", "banana", "orange", "coconut", "strawberry", "lime", "grapefruit", "watermelon", "blueberry", "blackberry", "raspberry"];
+  const words = [
+    "apple",
+    "banana",
+    "orange",
+    "coconut",
+    "strawberry",
+    "lime",
+    "grapefruit",
+    "watermelon",
+    "blueberry",
+    "blackberry",
+    "raspberry",
+  ];
   const randomIndex = Math.floor(Math.random() * words.length);
   return words[randomIndex];
 }
@@ -44,7 +58,9 @@ function getMistakeCount() {
 // Q: How many correct guesses has the user made so far?
 // Count the letters in state.guessedLetters that ARE in state.word
 function getCorrectGuesses() {
-  let correctGuesses = state.characters.filter((char) => state.word.includes(char));
+  let correctGuesses = state.characters.filter((char) =>
+    state.word.includes(char)
+  );
   return correctGuesses;
 }
 function getCorrectGuessesCount() {
@@ -53,13 +69,11 @@ function getCorrectGuessesCount() {
 }
 // Q: Has the user won?
 function checkIfUserWon(): boolean {
-  // are all the letters in state.word in state.guessedLetters?
-  if (getCorrectGuessesCount() === state.word.length) {
-    return true; // user won
-
-  } else {
-    return false; // user lost
+  // are all the letters in state.word in state.characters?
+  for (let char of state.word) {
+    if (!state.characters.includes(char)) return false;
   }
+  return true;
 }
 
 // Q: Has the user lost?
@@ -67,24 +81,32 @@ function checkIfUserLost(): boolean {
   // is the user out of attempts?
   return true;
 }
+function listenToUserKeyPresses() {
+  document.addEventListener("keyup", function (event) {
+    let guess = event.key.toLowerCase();
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    if (!letters.includes(guess)) return;
 
+    if (state.characters.includes(guess)) return;
+
+    state.characters.push(guess);
+    render();
+  });
+}
 function render() {
   let appEl = document.querySelector("#app");
   if (appEl === null) return;
+  appEl.textContent = "";
   let mistakesH3El = document.createElement("h3");
   mistakesH3El.className = "mistakes";
   mistakesH3El.textContent = `Wrong guesses: ${getMistakes()} (${getMistakeCount()})`;
-  
+
   let correctGuessesH3El1 = document.createElement("h3");
   correctGuessesH3El1.className = "correctGuesses";
   correctGuessesH3El1.textContent = `Correct guesses: ${getCorrectGuesses()} (${getCorrectGuessesCount()})`;
 
   appEl.append(mistakesH3El, correctGuessesH3El1);
 }
+
+listenToUserKeyPresses();
 render();
-window.state = state;
-window.getMistakeCount = getMistakeCount;
-window.getMistakes = getMistakes;
-window.getCorrectGuesses = getCorrectGuesses;
-window.getCorrectGuessesCount = getCorrectGuessesCount;
-window.getRandomWord = getRandomWord;
